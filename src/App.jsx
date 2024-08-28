beb823d423bb8c9ef031d08eb418f8021594f58b
dianiputri03 <dianiputri930@gmail.com>
2024-08-28 14:18:43 +0700

    add menubar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Menubar from "./components/Menubar"
 import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen">
       <Navbar/>
+      <Menubar/>
     </div>
   )
 }
